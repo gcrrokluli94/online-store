50a4e771c3e3f900cb8825733e7888ca6ed65fa4
Rudina Demi <rudinademi@yahoo.com>
2021-02-07 20:55:29 +0100

    Initial Commit

---
+++ src/main/java/online/store/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package online.store.repositories;
+
+import online.store.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+}
